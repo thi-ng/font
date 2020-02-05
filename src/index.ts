051eff81d7d0659086a1d531a88870ff2dc88752
Alberto Massa <alberto.massa@ymail.com>
2020-02-05 21:48:42 +0100

    feat(glyph): capital letters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { map, mapcat } from "@thi.ng/transducers";
+import { map } from "@thi.ng/transducers";
 import { readFileSync, writeFileSync } from "fs";
-import { Font, Glyph, Path } from "opentype.js";
+import { Font } from "opentype.js";
 import { COL_WIDTH, MAX_Y, MIN_Y, R } from "./api";
 import { defGlyph } from "./gen";
 import * as pkg from "../package.json";
@@ -43,47 +43,63 @@ const glyphs = [
         { id: 0x36, g: "5a/aeh44>5a" },
         { id: 0x37, g: "/4dhd4>" },
         { id: 0x38, g: "48ae/8a/a8h44hd4>48ae" },
-        { id: 0x39, g: "9dhd4/49>9d" }
-    ]),
-    ...mapcat(
-        (spec) => [
-            // orig
-            defGlyph(spec),
-            // duplicate as lowercase
-            defGlyph({ ...spec, id: spec.id + 0x20 })
-        ],
-        [
-            { id: 0x41, g: "58h44h94>59" },
-            { id: 0x42, g: "5eh44>5a" },
-            { id: 0x43, g: "5ah44h94>5689" },
-            { id: 0x44, g: "5ah44>5e" },
-            { id: 0x45, g: "5ah44h94>69" },
-            { id: 0x46, g: "49adh93hd4>cd", width: 2 * COL_WIDTH - R },
-            { id: 0x47, g: "4912h04h94>19" },
-            { id: 0x48, g: "49aeh94>49" },
-            { id: 0x49, g: "4a.b" },
-            { id: 0x4a, g: "12h04H93>19.b", x: -R },
-            { id: 0x4b, g: "4e/78/76>468a" },
-            { id: 0x4c, g: "4e" },
-            { id: 0x4d, g: "49h96>49>49" },
-            { id: 0x4e, g: "49h94>49" },
-            { id: 0x4f, g: "59h44h94>59" },
-            { id: 0x50, g: "09h94>49" },
-            { id: 0x51, g: "49h94>09" },
-            { id: 0x52, g: "49h94>89" },
-            { id: 0x53, g: "5689/86h44h94>5689" },
-            { id: 0x54, g: "Hb3>4bce", x: -COL_WIDTH },
-            { id: 0x55, g: "5ah44>5a" },
-            { id: 0x56, g: "4a/46>6a" },
-            { id: 0x57, g: "5ah46>5a>5a" },
-            { id: 0x58, g: "468a/68/86>468a" },
-            { id: 0x59, g: "5ah44>045a" },
-            { id: 0x5a, g: "/59h44h94>" }
-        ]
-    )
-];
+        { id: 0x39, g: "9dhd4/49>9d" },
 
-console.log({ glyphs }, glyphs[38], (glyphs[38].path as Path).toSVG(4));
+        { id: 0x41 + 0x20, g: "4eHd3H83>4e" }, // A
+        { id: 0x42 + 0x20, g: "48ae8aHd3H43/89/98>489d" }, // B
+        { id: 0x43 + 0x20, g: "4eHd3H43>" }, //C
+        { id: 0x44 + 0x20, g: "4e/ed/45>5d" }, // D
+        { id: 0x45 + 0x20, g: "4eH43H82Hd3>" }, // E
+        { id: 0x46 + 0x20, g: "4eH82Hd3>" }, // F
+        { id: 0x47 + 0x20, g: "4eH43Hd3>49" }, // G
+        { id: 0x48 + 0x20, g: "4eH83>4e" }, // H
+        { id: 0x49 + 0x20, g: "4e>" }, // I
+        { id: 0x4a + 0x20, g: "H42>4e", x: -COL_WIDTH }, // J
+        { id: 0x4b + 0x20, g: "48ae8a/89/98>489e" }, // K
+        { id: 0x4c + 0x20, g: "4eH43>" }, // L
+        { id: 0x4d + 0x20, g: "4eHd3>Hd38e>4e" }, // M
+        { id: 0x4e + 0x20, g: "4eHd3>4e" }, // N
+        { id: 0x4f + 0x20, g: "4eHd3H43>4e" }, // O
+        { id: 0x50 + 0x20, g: "4eHd3H83>8e" }, // P
+        { id: 0x51 + 0x20, g: "4eHd3H43>4eH42>" }, // Q
+        { id: 0x52 + 0x20, g: "4eHd3/89/98>9e48" }, // R
+        { id: 0x53 + 0x20, g: "8eHd3H83h43>48" }, // S
+        { id: 0x54 + 0x20, g: "hd3>4eHd3>" }, // T
+        { id: 0x55 + 0x20, g: "4eH43>4e" }, // U
+        { id: 0x56 + 0x20, g: "4e/45>5e" }, // V
+        { id: 0x57 + 0x20, g: "4eH43>H4349>4e" }, // W
+        { id: 0x58 + 0x20, g: "489e/89/98>489e" }, // X
+        { id: 0x59 + 0x20, g: "8eH83>4e" }, // Y
+        { id: 0x5a + 0x20, g: "hd4H43/4d" }, // Z
+
+        { id: 0x41, g: "58h44h94>59" },
+        { id: 0x42, g: "5eh44>5a" },
+        { id: 0x43, g: "5ah44h94>5689" },
+        { id: 0x44, g: "5ah44>5e" },
+        { id: 0x45, g: "5ah44h94>69" },
+        { id: 0x46, g: "49adh93hd4>cd", width: 2 * COL_WIDTH - R },
+        { id: 0x47, g: "4912h04h94>19" },
+        { id: 0x48, g: "49aeh94>49" },
+        { id: 0x49, g: "4a.b" },
+        { id: 0x4a, g: "12h04H93>19.b", x: -R },
+        { id: 0x4b, g: "4e/78/76>468a" },
+        { id: 0x4c, g: "4e" },
+        { id: 0x4d, g: "49h96>49>49" },
+        { id: 0x4e, g: "49h94>49" },
+        { id: 0x4f, g: "59h44h94>59" },
+        { id: 0x50, g: "09h94>49" },
+        { id: 0x51, g: "49h94>09" },
+        { id: 0x52, g: "49h94>89" },
+        { id: 0x53, g: "5689/86h44h94>5689" },
+        { id: 0x54, g: "Hb3>4bce", x: -COL_WIDTH },
+        { id: 0x55, g: "5ah44>5a" },
+        { id: 0x56, g: "4a/46>6a" },
+        { id: 0x57, g: "5ah46>5a>5a" },
+        { id: 0x58, g: "468a/68/86>468a" },
+        { id: 0x59, g: "5ah44>045a" },
+        { id: 0x5a, g: "/59h44h94>" }
+    ])
+];
 
 const font = new Font({
     ...pkg.font,
